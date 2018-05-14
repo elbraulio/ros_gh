0c07373c487066cc0d11a57aae7ac6797cec09f4
elbraulio <brauliop.3@gmail.com>
2018-05-14 12:11:54 -0400

    update docs

--- src/main/java/iterator/IterateUserPages.java
+++ src/main/java/iterator/IterateUserPages.java
@@ -9,13 +9,35 @@ import java.util.NoSuchElementException;
 import java.util.Stack;
 
 /**
+ * Iterates pages from {@link PagedDom} and retrieves user links from each page.
+ *
  * @author Braulio Lopez (brauliop.3@gmail.com)
  */
 public final class IterateUserPages implements Iterator<IterateUserLinks> {
+    /**
+     * The first value from the stack represent the current iterated page.
+     * Just for keep the immutability of this class, by using this instead of
+     * a mutable int that stores the current page on the iteration.
+     */
     private final Stack<Integer> currentPage;
+
+    /**
+     * The paged dom that will be iterated.
+     */
     private final PagedDom pagedDom;
+
+    /**
+     * Last page to iterate.
+     */
     private final int lastPage;
 
+    /**
+     * Ctor.
+     *
+     * @param pagedDom    The paged dom that will be iterated.
+     * @param initialPage Page where the iteration begins.
+     * @param finalPage   Page where the iteration ends.
+     */
     public IterateUserPages(
             final PagedDom pagedDom, final int initialPage, final int finalPage
     ) {
@@ -26,6 +48,13 @@ public final class IterateUserPages implements Iterator<IterateUserLinks> {
         );
     }
 
+    /**
+     * Ctor.
+     * This Ctor. hides the {@link Stack} from the user.
+     * @param withInitialPage
+     * @param pagedDom
+     * @param lastPage
+     */
     private IterateUserPages(
             final Stack<Integer> withInitialPage,
             final PagedDom pagedDom,
