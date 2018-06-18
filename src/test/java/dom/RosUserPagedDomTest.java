dbfecc3ae730455551c5ea539ef951e682e74e05
elbraulio <brauliop.3@gmail.com>
2018-06-18 10:18:45 -0400

    refactor

--- src/test/java/dom/RosUserPagedDomTest.java
+++ src/test/java/dom/RosUserPagedDomTest.java
@@ -11,12 +11,12 @@ import static org.hamcrest.core.Is.is;
 /**
  * @author Braulio Lopez (brauliop.3@gmail.com)
  */
-public final class RosPagedDomTest {
+public final class RosUserPagedDomTest {
     @Test
     @Ignore
     public void fetchWebDom() throws IOException {
         assertThat(
-                new RosPagedDom()
+                new RosUserPagedDom()
                         .page(1)
                         .select("title").text(),
                 is("Users - ROS Answers: Open Source Q&A Forum")
