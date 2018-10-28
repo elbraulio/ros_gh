66f6f53e0a5b5e266e1b1b93998b480234d61be2
elbraulio <brauliop.3@gmail.com>
2018-10-28 18:48:07 -0300

    build ruu with jaccard similarity
    #7

--- src/main/java/examples/devrec/Devrec.java
+++ src/main/java/examples/devrec/Devrec.java
@@ -36,7 +36,6 @@ public class Devrec implements Algorithm {
             Map<Integer, Integer> users = new FetchIndexedUsers(
                     connection
             ).map();
-
             /*
              * create Rup association matrix filled with 0's.
              * ----p1-p2- ... -pn
@@ -44,8 +43,9 @@ public class Devrec implements Algorithm {
              * |u2
              * |...
              * |un
+             * then fill with 1's when a user collaborates in a repository.
              */
-            int[][] rup = new int[users.size()][projects.size()];
+            double[][] rup = new double[users.size()][projects.size()];
             for (int userId : users.keySet()) {
                 int userIndex = users.get(userId);
                 List<Integer> collaborations = new FetchIndexProjectsByUser(
@@ -57,16 +57,60 @@ public class Devrec implements Algorithm {
                 }
             }
             this.logger.info("Rup matrix:\n" + printMatrix(rup));
+            /*
+             * create Ruu association matrix filled with 0's. This matrix is
+             * symmetric.
+             */
+            double[][] ruu = new double[users.size()][users.size()];
+            for (int i = 0; i < ruu.length; i++) {
+                for (int j = i; j < ruu.length; j++) {
+                    ruu[i][j] = jaccardSimilarity(i, j, rup);
+                    ruu[j][i] = jaccardSimilarity(i, j, rup);
+                }
+            }
+            this.logger.info("Ruu matrix:\n" + printMatrix(ruu));
         } catch (Exception e) {
             this.logger.error("e", e);
         }
         return aspirants;
     }
 
-    private String printMatrix(int[][] matrix) {
+    /**
+     * Compare similarity between user i toward user j.
+     *
+     * @param i   user to compared
+     * @param j   user to compare with
+     * @param rup user project relation matrix.
+     * @return jaccard index between user i and j.
+     */
+    private double jaccardSimilarity(int i, int j, double[][] rup) {
+        double[] projectsI = rup[i];
+        double[] projectsJ = rup[j];
+        double sum = 0d;
+        double sampleI = 0d;
+        double sampleJ = 0d;
+        for (int k = 0; k < projectsI.length; k++) {
+            sum += projectsI[k] + projectsJ[k] == 2d ? 1 : 0;
+            sampleI += projectsI[k];
+            sampleJ += projectsJ[k];
+        }
+        if (sampleI + sampleJ == 0) {
+            return 0;
+        } else {
+            return sum / (sampleI + sampleJ);
+        }
+    }
+
+    /**
+     * print a matrix.
+     *
+     * @param matrix to be printed.
+     * @return string representing the matrix.
+     */
+    private String printMatrix(double[][] matrix) {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < matrix.length; i++) {
-            int[] row = matrix[i];
+            double[] row = matrix[i];
             for (int j = 0; j < row.length; j++) {
                 builder.append(row[j]);
             }
