8d49ea1a9324e481e7cc64c49cd15bcbac601029
elbraulio <brauliop.3@gmail.com>
2018-11-11 13:48:45 -0300

    calculate rut matrix

--- src/main/java/examples/devrec/Launcher.java
+++ src/main/java/examples/devrec/Launcher.java
@@ -1,5 +1,6 @@
 package examples.devrec;
 
+import org.apache.log4j.Logger;
 import org.elbraulio.rosgh.algorithm.Algorithm;
 import org.elbraulio.rosgh.algorithm.Aspirant;
 import org.elbraulio.rosgh.algorithm.ByRankDesc;
@@ -10,11 +11,14 @@ import java.util.List;
  * @author Braulio Lopez (brauliop.3@gmail.com)
  */
 public class Launcher {
+
+    private static final Logger logger = Logger.getLogger(Launcher.class);
+
     public static void main(String... args) {
         Algorithm devrec = new Devrec();
         List<Aspirant> sorted = new ByRankDesc().orderedList(
                 devrec.aspirants(null)
         );
-        System.out.println(sorted);
+        logger.info(sorted);
     }
 }
