d5b3303f8981eb6c8bb1ccf38170321a191630db
elbraulio <brauliop.3@gmail.com>
2018-12-15 03:24:54 -0300

    fix devrec

---
+++ src/main/java/com/elbraulio/rosgh/health/FetchTagByUser.java
@@ -0,0 +1,49 @@
+package com.elbraulio.rosgh.health;
+
+
+import com.elbraulio.rosgh.tag.RosTag;
+import com.elbraulio.rosgh.tag.Tag;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Braulio Lopez (brauliop.3@gmail.com)
+ */
+public final class FetchTagByUser {
+    private final int id;
+    private final Connection connection;
+
+    public FetchTagByUser(int id, Connection connection) {
+        this.id = id;
+        this.connection = connection;
+    }
+
+    public List<Tag> list() throws SQLException {
+        String userExists = "select distinct ros_tag.* from main.ros_tag join" +
+                " ros_user_tag " +
+                "rut " +
+                " on ros_tag.id = rut.ros_tag_id join linked_users lu on rut" +
+                ".ros_user_id=lu.ros_user_id where lu" +
+                ".gh_user_id=" + this.id + ";";
+        List<Tag> tags = new LinkedList<>();
+        try (
+                Statement stmt = connection.createStatement();
+                ResultSet rs = stmt.executeQuery(userExists)
+        ) {
+            while (rs.next()) {
+                tags.add(
+                        new RosTag(
+                                rs.getString("name"),
+                                rs.getInt("id")
+                        )
+                );
+            }
+        }
+        return tags;
+    }
+}
